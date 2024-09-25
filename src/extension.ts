883c2f6337c6dff1bfdf2d6039f2893cdcd9defc
kvoon <a1176079417@gmail.com>
2024-09-25 11:39:38 +0800

    feat: detect window bash

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,33 @@
+import type { Terminal } from 'vscode'
 import { platform } from 'node:os'
 import { window, workspace } from 'vscode'
 
 const isWindows = platform() === 'win32'
 
-const COMMAND = isWindows
-  ? '$env:PATH = "{0}\\node_modules\\.bin;{1}" -f (Resolve-Path .), $env:PATH'
-  : 'export PATH=$PWD/node_modules/.bin:$PATH'
+function resolveCmd(t: Terminal) {
+  const winCmd = '$env:PATH = "{0}\\node_modules\\.bin;{1}" -f (Resolve-Path .), $env:PATH'
+  const unixCmd = 'export PATH=$PWD/node_modules/.bin:$PATH'
+
+  if (!isWindows)
+    return unixCmd
+
+  // @ts-expect-error type error
+  if ([/bash/, /zsh/].some(re => re.test(t.creationOptions?.shellPath)))
+    return unixCmd
+  else
+    return winCmd
+}
 
 function setup() {
   window.terminals.forEach(async (t) => {
     if (await t.processId)
       return
-    t.sendText(COMMAND)
+    t.sendText(resolveCmd(t))
   })
   window.onDidOpenTerminal((t) => {
-    t.sendText(COMMAND)
+    // window.createOutputChannel('ttt', JSON.stringify(t))
+    t.sendText(`echo ${JSON.stringify(t)}`)
+    t.sendText(resolveCmd(t))
   })
 }
 
